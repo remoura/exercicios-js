4414c83cd652225b4e65ca7f363a202aebf9e7c0
Renan O. Moura <65793359+remoura@users.noreply.github.com>
2020-06-19 16:18:47 -0300

    Template String

--- Fundamentos/Numerosalgunscuidados.js
+++ Fundamentos/Numerosalgunscuidados.js
@@ -1,6 +1,6 @@
 console.log(7 / 0)
 console.log("10" / 2)
-console.log('3' + 2)// faz sentido no mundo da string
+console.log('3' + 2)// a string ganha
 console.log('3' - 2)
 console.log("show!" * 2)
 console.log(0.1 + 0.7)
